f0d52ccf0de07bb61d547ccfda8b48c00f5d1e0d
smnshuvo <smnshuvo@gmail.com>
2020-06-08 17:56:27 +0600

    Insert operation

--- app/src/main/java/cpc/class_planner/sam/data/RoutineDao.java
+++ app/src/main/java/cpc/class_planner/sam/data/RoutineDao.java
@@ -1,4 +1,13 @@
 package cpc.class_planner.sam.data;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+import cpc.class_planner.sam.model.Routine;
+
+@Dao
 public interface RoutineDao {
+
+    @Insert
+    void insertData(Routine routine);
 }
