d09e8e196a9ce84eb10d7a1abcd6cdcf545c0f4b
smnshuvo <smnshuvo@gmail.com>
2020-06-10 20:22:06 +0600

    BaseActivity Shouldn't take input

---
+++ app/src/main/java/cpc/class_planner/sam/view/InputRoutineActivity.java
@@ -0,0 +1,16 @@
+package cpc.class_planner.sam.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cpc.class_planner.sam.R;
+
+public class InputRoutineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_routine);
+    }
+}
--- app/src/main/java/cpc/class_planner/sam/viewmodel/InputActivityViewModel.java
+++ app/src/main/java/cpc/class_planner/sam/viewmodel/InputActivityViewModel.java
@@ -1,34 +1,21 @@
 package cpc.class_planner.sam.viewmodel;
 
 import android.app.Application;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
-import java.util.List;
-
 import cpc.class_planner.sam.data.RoutineDao;
 import cpc.class_planner.sam.data.RoutineDatabase;
-import cpc.class_planner.sam.model.Routine;
 
-public class BaseActivityViewModel extends AndroidViewModel {
-
+public class InputActivityViewModel extends AndroidViewModel {
     private String TAG = this.getClass().getSimpleName();
     private RoutineDao routineDao;
     private RoutineDatabase database;
 
-    // base constructor for android ViewModel
-    public BaseActivityViewModel(@NonNull Application application) {
+    public InputActivityViewModel(@NonNull Application application) {
         super(application);
         database = RoutineDatabase.getInstance(application);
         routineDao = database.routineDao();
     }
-
-
-    public List<Routine> getDailyRoutine(final String day){
-       return routineDao.getTodayRoutine(day);
-    }
-
 }
