dcb89c88fc6f5d061139446510cd33a625e6f984
smnshuvo <smnshuvo@gmail.com>
2020-06-11 23:24:23 +0600

    Designed the routine item view

--- app/src/main/java/cpc/class_planner/sam/viewmodel/InputActivityViewModel.java
+++ app/src/main/java/cpc/class_planner/sam/viewmodel/InputActivityViewModel.java
@@ -1,12 +1,14 @@
 package cpc.class_planner.sam.viewmodel;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
 import cpc.class_planner.sam.data.RoutineDao;
 import cpc.class_planner.sam.data.RoutineDatabase;
+import cpc.class_planner.sam.model.Routine;
 
 public class InputActivityViewModel extends AndroidViewModel {
     private String TAG = this.getClass().getSimpleName();
@@ -17,5 +19,17 @@ public class InputActivityViewModel extends AndroidViewModel {
         super(application);
         database = RoutineDatabase.getInstance(application);
         routineDao = database.routineDao();
+    }
+
+    // for smooth experience we will not run this on main thread
+    public void insert(final Routine routine){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                routineDao.insertData(routine);
+                Log.d(TAG, "run: Inserted " + routine.getCourseTitle());
+            }
+        });
+        thread.start();
     }
 }
